27c91771ba108279b33258bf0297ccb2751122ff
cPanelKevin <kmitsch@cpanel.net>
2018-04-05 10:56:17 -0500

    Updated and partially rewrote php_set_vhost_versions.pl

--- php_set_vhost_versions.pl
+++ php_set_vhost_versions.pl
@@ -1,14 +1,23 @@
 #!/usr/local/cpanel/3rdparty/bin/perl
 
-# this script will generate an array of domains effected by a PHP configuration change
-
-# install the script using:
-## /usr/local/cpanel/bin/manage_hooks add script /root/scripts/inputdumper.pl --manual --category Whostmgr --event 'Accounts::Create' --stage post
-# adjust the script path '/root/scripts/inputdumper.pl' if needed
-# set the execute bit on the script
-## chmod +x /root/scripts/inputdumper.pl
-# test using:
-## whmapi1 php_set_vhost_versions vhost-0=domain1.test vhost-1=domain2.test
+# //////////////////////////////////////////////////////
+# This script will generate an array of domains effected by a PHP configuration change,
+# such as when a change is made in MultiPHP Manager.
+# Install the script using:
+#
+# /usr/local/cpanel/bin/manage_hooks add script /root/scripts/setvhostver.pl --manual --category Whostmgr --event 'Lang::PHP::set_vhost_versions' --stage post
+#
+# Adjust the script path '/root/scripts/setvhostver.pl' if needed
+# Set the execute bit on the script:
+#
+# chmod +x /root/scripts/setvhostver.pl
+#
+# Test using:
+#
+# whmapi1 php_set_vhost_versions vhost-0=cptech1.test vhost-1=cptech2.test
+#
+# Check the data provided by the script by viewing /root/scripts/setvhostver.log
+# //////////////////////////////////////////////////////
 
 use warnings;
 use strict;
@@ -21,7 +30,7 @@ my $json  = decode_json $input;
 my $data  = $json->{'data'};
 
 # adjust path to logfile if needed
-my $logfile = '/root/scripts/input.log';
+my $logfile = '/root/scripts/setvhostver.log';
 open( my $fh, '>', $logfile )
   or die "Could not open file: $logfile $!\n";
 
